df1215f01f465b39a788ece72ddd16e95c0ea9a4
unknown <fatmouse0814@gmail.com>
2022-02-24 23:50:01 +0800

    2022/2/24

---
+++ SkillTree/database.h
@@ -0,0 +1,8 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+class DataBase{
+    public:
+};
+
+#endif // DATABASE_H
--- build-SkillTree-Desktop_Qt_5_9_9_MinGW_32bit-Debug/ui_mainwindow.h
+++ build-SkillTree-Desktop_Qt_5_9_9_MinGW_32bit-Debug/ui_mainwindow.h
@@ -13,9 +13,11 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -25,6 +27,10 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton_store;
+    QPushButton *pushButton_bag;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -33,11 +39,45 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
         MainWindow->resize(800, 600);
+        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setMinimumSize(QSize(800, 600));
+        MainWindow->setMaximumSize(QSize(800, 600));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QStringLiteral("centralwidget"));
+        sizePolicy.setHeightForWidth(centralwidget->sizePolicy().hasHeightForWidth());
+        centralwidget->setSizePolicy(sizePolicy);
+        horizontalLayoutWidget = new QWidget(centralwidget);
+        horizontalLayoutWidget->setObjectName(QStringLiteral("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(590, 490, 169, 80));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton_store = new QPushButton(horizontalLayoutWidget);
+        pushButton_store->setObjectName(QStringLiteral("pushButton_store"));
+        sizePolicy.setHeightForWidth(pushButton_store->sizePolicy().hasHeightForWidth());
+        pushButton_store->setSizePolicy(sizePolicy);
+        pushButton_store->setMinimumSize(QSize(80, 40));
+        pushButton_store->setMaximumSize(QSize(80, 40));
+
+        horizontalLayout->addWidget(pushButton_store);
+
+        pushButton_bag = new QPushButton(horizontalLayoutWidget);
+        pushButton_bag->setObjectName(QStringLiteral("pushButton_bag"));
+        sizePolicy.setHeightForWidth(pushButton_bag->sizePolicy().hasHeightForWidth());
+        pushButton_bag->setSizePolicy(sizePolicy);
+        pushButton_bag->setMinimumSize(QSize(80, 40));
+        pushButton_bag->setMaximumSize(QSize(80, 40));
+
+        horizontalLayout->addWidget(pushButton_bag);
+
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QStringLiteral("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 26));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QStringLiteral("statusbar"));
@@ -51,6 +91,8 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        pushButton_store->setText(QApplication::translate("MainWindow", "Store", Q_NULLPTR));
+        pushButton_bag->setText(QApplication::translate("MainWindow", "Bag", Q_NULLPTR));
     } // retranslateUi
 
 };
